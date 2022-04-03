f758a7f45dfe6834750a5b5be49f4bdbbf4b86cb
Helium1307 <heliofilho1307@hotmail.com>
2022-04-03 13:55:07 -0300

    marked based

--- pages/index.js
+++ pages/index.js
@@ -16,26 +16,14 @@ export default function Home() {
       </Head>
 
       <main>
-        <a-scene
-          vr-mode-ui="enabled: false;"
-          renderer="logarithmicDepthBuffer: true;"
-          embedded
-          arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
-        >
-          <a-nft
-            type="nft"
-            url="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex"
-            smooth="true"
-            smoothCount="10"
-            smoothTolerance=".01"
-            smoothThreshold="5"
-          >
+        <a-scene embedded arjs>
+          <a-marker preset="hiro">
             <a-entity
+              position="0 0 0"
+              scale="0.05 0.05 0.05"
               gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-              scale="5 5 5"
-              position="50 150 0"
             ></a-entity>
-          </a-nft>
+          </a-marker>
           <a-entity camera></a-entity>
         </a-scene>
       </main>
